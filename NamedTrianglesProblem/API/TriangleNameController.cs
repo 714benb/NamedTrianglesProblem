8db7acbbb02ce95854a97725204bd31c94c1d55f
MithralSmith <TheSmith@MithralSmith>
2020-09-27 16:32:00 -0700

    Added TriangleNameController and modified launchSettings to call it on startup.

---
+++ NamedTrianglesProblem/API/TriangleNameController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NamedTrianglesProblem.Models;
+
+namespace NamedTrianglesProblem.API
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class TriangleNameController : ControllerBase
+  {
+    // GET API/triangleName?v1.X=0&v1.Y=0&v2.X=10&v2.Y=10&v3.X=0&v3.Y=10
+    [HttpGet()]
+    public string Get([FromQuery] Vertex v1, [FromQuery] Vertex v2, [FromQuery] Vertex v3)
+    {
+      var triangle = Triangle.Create(v1, v2, v3);
+
+      return triangle.CalculateName();
+    }
+  }
+}
