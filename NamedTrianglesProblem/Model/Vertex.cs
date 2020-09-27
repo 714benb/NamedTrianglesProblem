553f15944787226fdd316ea72b54527e39c7c843
MithralSmith <TheSmith@MithralSmith>
2020-09-27 14:21:17 -0700

    Created the project and solution using Visual Studio 19 project ASP.NET MVC project template. Added the Vertex class and tests

---
+++ NamedTrianglesProblem/Model/Vertex.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NamedTrianglesProblem.Models
+{
+  public class Vertex : IEquatable<Vertex>, IEqualityComparer<Vertex>
+  {
+    /// <summary>
+    /// The X coordinate of the Vertex
+    /// </summary>
+    public int X { get; set; }
+
+    /// <summary>
+    /// The Y coordinate of the Vertex
+    /// </summary>
+    public int Y { get; set; }
+
+    /// <summary>Returns a string that represents the vertex as a (x,y) pair.</summary>
+    /// <returns>A string for the (x,y) coordinates of the Vertex.</returns>
+    /// <remarks>Overriding the ToString makes debugging and interpolation much easier.</remarks>
+    public override string ToString()
+    {
+      return $"({X},{Y})";
+    }
+
+    #region IEquatable<Vertex> implementation
+
+    /// <summary>Indicates whether the current object is equal to another object of the same type.</summary>
+    /// <param name="other">An object to compare with this object.</param>
+    /// <returns>
+    /// <see langword="true" /> if the current object is equal to the <paramref name="other" /> parameter; otherwise, <see langword="false" />.</returns>
+    public bool Equals(Vertex other)
+    {
+      if (ReferenceEquals(null, other)) return false;
+      if (ReferenceEquals(this, other)) return true;
+      return X == other.X && Y == other.Y;
+    }
+
+    /// <summary>Determines whether the specified object is equal to the current object.</summary>
+    /// <param name="obj">The object to compare with the current object.</param>
+    /// <returns>
+    /// <see langword="true" /> if the specified object  is equal to the current object; otherwise, <see langword="false" />.</returns>
+    public override bool Equals(object obj)
+    {
+      if (ReferenceEquals(null, obj)) return false;
+      if (ReferenceEquals(this, obj)) return true;
+      if (obj.GetType() != this.GetType()) return false;
+      return Equals((Vertex) obj);
+    }
+
+    /// <summary>Serves as the default hash function.</summary>
+    /// <returns>A hash code for the current object.</returns>
+    public override int GetHashCode()
+    {
+      return HashCode.Combine(X, Y);
+    }
+    #endregion IEquatable<Vertex> implementation
+
+    #region  IEqualityComparer<Vertex> implementation
+    /// <summary>
+    /// Equals comparer for two Vertex objects 
+    /// </summary>
+    /// <param name="x"></param>
+    /// <param name="y"></param>
+    /// <returns></returns>
+    public bool Equals(Vertex x, Vertex y)
+    {
+      if (ReferenceEquals(x, y)) return true;
+      if (ReferenceEquals(x, null)) return false;
+      if (ReferenceEquals(y, null)) return false;
+      if (x.GetType() != y.GetType()) return false;
+      return x.X == y.X && x.Y == y.Y;
+    }
+
+    /// <summary>
+    /// Get the hash code of a Vertex object
+    /// </summary>
+    /// <param name="obj"></param>
+    /// <returns></returns>
+
+    public int GetHashCode(Vertex obj)
+    {
+      return obj.GetHashCode();
+    }
+    #endregion  IEqualityComparer<Vertex>
+    public static bool operator == (Vertex v1, Vertex v2)
+    {
+      return (v1?.Equals(v2)??false);
+    }
+
+    public static bool operator != (Vertex v1, Vertex v2)
+    {
+      return (v1?.Equals(v2) ?? false) == false;
+    }
+
+
+  }
+}
---
+++ NamedTrianglesProblemTests/Model/VertexTests.cs
@@ -0,0 +1,102 @@
+﻿using NUnit.Framework;
+using NamedTrianglesProblem.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NamedTrianglesProblem.Models.Tests
+{
+  [TestFixture()]
+  public class VertexTests
+  {
+
+    //Note:  These are trivial tests that I wouldn't normally write but I want to
+    //       demonstrate my process which is to create simple tests then create
+    //       data driven tests or even integration tests driven from the unit test runner.
+    [Test()]
+    public void Equals_ReturnsTrueWhen_PropertiesAreEqual_Test()
+    {
+      //Arrange
+      var v1 = new Vertex() {X=1, Y=1};
+      var v2 = new Vertex() {X=1, Y=1};
+      //Act
+      var actual = v1.Equals(v2);
+      //Assert
+      Assert.That(actual, Is.True);
+    }
+
+    [Test()]
+    public void Equals_ReturnsFalseWhen_PropertiesAreNotEqual_Test()
+    {
+      //Arrange
+      var v1 = new Vertex() { X = 1, Y = 2 };
+      var v2 = new Vertex() { X = 1, Y = 1 };
+      //Act
+      var actual = v1.Equals(v2);
+      //Assert
+      Assert.That(actual, Is.False);
+    }
+
+    //NOTE: I wrote the operator definitions as opposed to using Resharper to generate
+    //      the IEquatable<Vertex> and IEqualityComparer<Vertex> implementations.  They
+    //      are more deserving of tests, though they are still trivial.
+    [Test()]
+    public void EqualsOperator_ReturnsTrueWhen_PropertiesAreEqual_Test()
+    {
+      //Arrange
+      var v1 = new Vertex() { X = 1, Y = 1 };
+      var v2 = new Vertex() { X = 1, Y = 1 };
+      //Act
+      var actual = v1 == v2;
+      //Assert
+      Assert.That(actual, Is.True);
+    }
+
+    [Test()]
+    public void EqualsOperator_ReturnsFalseWhen_PropertiesAreNotEqual_Test()
+    {
+      //Arrange
+      var v1 = new Vertex() { X = 1, Y = 2 };
+      var v2 = new Vertex() { X = 1, Y = 1 };
+      //Act
+      var actual = v1 == v2;
+      //Assert
+      Assert.That(actual, Is.False);
+    }
+
+    private static object[] VertexPairs =
+    {
+      new object[] {new Vertex() {X = 0, Y = 0}, new Vertex() {X = 0, Y = 0}, true},
+      new object[] {new Vertex() {X = 0, Y = 0}, new Vertex() {X = 1, Y = 0}, false},
+      new object[] {new Vertex() {X = 0, Y = 0}, new Vertex() {X = 0, Y = 1}, false},
+      new object[] {new Vertex() {X = 1, Y = 0}, new Vertex() {X = 0, Y = 0}, false},
+      new object[] {new Vertex() {X = 0, Y = 1}, new Vertex() {X = 0, Y = 0}, false},
+    };
+
+    [Test, TestCaseSource("VertexPairs")]
+    public void EqualsOperator_ReturnsExpectedValue_Test(Vertex v1, Vertex v2,bool expected)
+    {
+      //Arrange
+      //Act
+      var actual = v1 == v2;
+      //Assert
+      Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    //NOTE: It may not be advisable to use this "notExpected" pattern unless the team/company
+    //      agrees the shortcut (i.e. not needing to create a new list of values) is worth the
+    //      added confusion.   It does, however have the added benefit of testing the same values
+    //      which could help mitigate typo testing errors (particularly if the list is long or 
+    //      complicated.
+
+    [Test, TestCaseSource("VertexPairs")]
+    public void NotEqualsOperator_ReturnsExpectedValue_Test(Vertex v1, Vertex v2, bool notExpected)
+    {
+      //Arrange
+      //Act
+      var actual = v1 != v2;
+      //Assert
+      Assert.That(actual, Is.Not.EqualTo(notExpected));
+    }
+  }
+}
